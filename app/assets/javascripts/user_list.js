8de133e34febedb3ceb42896fbba9d9975151f71
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-29 15:56:06 +0100

    fighting with js

--- app/assets/javascripts/user_list.js
+++ app/assets/javascripts/user_list.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-  $('.user').click(function () {
-    $('.user-blue').toggleClass('user');
+  $( ".user" ).first().addClass('user-blue');
+  $('.user-grey').click(function () {
+    $('.user-blue').removeClass('user-blue');
+    $('user').addClass('user-grey');
     $(this).removeClass('user');
     $(this).addClass('user-blue');
   });
