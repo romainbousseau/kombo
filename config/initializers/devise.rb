9c2a4edadf0fd1843b5d20df19cd45629f9dbd43
Cedric Njanga <ced.njg@gmail.com>
2016-11-29 15:07:18 +0100

    Linkedin connect finally, sorry guys it was way too long, fucking gem

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :linked_in, ENV["LINKEDIN_ID"], ENV["LINKEDIN_CLIENT"]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -272,4 +271,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :linkedin, ENV["LINKEDIN_ID"], ENV["LINKEDIN_CLIENT"], secure_image_url: true, :scope => "r_basicprofile r_emailaddress"
 end
--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linked_in, ENV["LINKEDIN_ID"], ENV["LINKEDIN_CLIENT"], 
-    :scope => "r_basicprofile r_emailaddress",
-    :field => ["id", "email-address"]
-end
