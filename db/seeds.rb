eefe172321c1f637df2728575e48824500c0973f
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-30 18:58:00 +0100

    scrap base ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "capybara"
+require "capybara/dsl"
+require 'capybara/poltergeist'
+
+class Scraper
+  include Capybara::DSL
+
+  def run
+    Capybara.default_max_wait_time = 15
+    # Capybara.match = :first
+    Capybara.configure do |c|
+      c.javascript_driver = :poltergeist
+      c.default_driver = :poltergeist
+    end
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, {
+        js_errors: false,
+        debug: false,
+        phantomjs_options: ['--debug=no', '--load-images=no', '--ignore-ssl-errors=yes', '--ssl-protocol=TLSv1'],
+        timeout: 45,
+        headers: { 'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36" }
+      })
+    end
+
+    Capybara.app_host = "http://www.linkedin.com"
+    page.driver.browser.clear_cookies
+    visit('/')
+    sleep 2
+    fill_in 'login-email', :with => 'jcjuppe@gmail.com', visible: false
+    fill_in 'login-password', :with => 'lewagon1', visible: false
+    click_on('login-submit')
+    sleep 2
+
+    visit('https://www.linkedin.com/directory/topics-more/')
+
+    all(".wrapper .section:nth-child(4) a").first.click
+    sleep 1
+    page.save_screenshot('screenshot.png')
+    title = []
+    all('.quad-column a').each do |link|
+      title << link.text
+    end
+    puts title
+  end
+end
+
+Scraper.new.run
