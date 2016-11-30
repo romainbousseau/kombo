423c25c6920343d30f4a869e546f0de4d05e8062
Maxence De Villepion <maxvillepion@gmail.com>
2016-11-30 17:40:22 +0100

    chosen set up : Gem chosen rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
 //= require moment
 //= require fullcalendar
+//= require chosen-jquery
+//= require_tree .
