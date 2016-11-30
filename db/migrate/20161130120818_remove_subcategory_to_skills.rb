3db1cb86fabc3c44ca3c5c8880b1ae14397765ee
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-30 13:33:44 +0100

    killed migrations

--- db/migrate/20161130120818_remove_subcategory_to_skills.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSubcategoryToSkills < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :skills, :subcategory
-  end
-end
--- db/migrate/20161130120856_remove_category_to_categoriess.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryToCategoriess < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :subcategories, :category
-  end
-end
