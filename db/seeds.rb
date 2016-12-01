d3da531480016417967a60ad569f5842ec0b2612
Cedric Njanga <ced.njg@gmail.com>
2016-12-01 11:04:00 +0100

    skills seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+skills = ["seo", "sea", "product manager", "analytics", "programmatic", "data analyst", "developer", "content manager", "revemue manager", "web designer"]
+
+skills.each {|skill| Skill.create!(name: skill)}
