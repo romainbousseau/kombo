76d33ea92fadeb2d282e2e2de98c96af0f49ed23
Cedric Njanga <ced.njg@gmail.com>
2016-12-02 01:04:14 +0100

    Youhou

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     @match = Match.new
     filtered_search = params[:skill][:name].reject{|skill| skill.blank?}
-    @users = policy_scope(User).tagged_with(filtered_search)
+    @users = policy_scope(User).tagged_with(filtered_search)match
   end
 
   def show
