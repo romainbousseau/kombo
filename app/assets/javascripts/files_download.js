17fa62769da8ba24466cac427b56e3a03d067d10
Bousseau Romain <romainbousseau1@gmail.com>
2016-12-07 18:29:52 +0100

    added jquery for attached files (pdf/xls/png)

--- app/assets/javascripts/files_download.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".brief-text .fr-file").each(function(index, item) {
-    var href = $(item).attr("href");
-    var text = $(item).text();
-    console.log(href);
-    console.log(text);
-    $(item).hide();
-  });
-});
