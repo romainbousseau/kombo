fd9be0b98c445b4d4cdbd516be5b018bf8bf5d32
Bousseau Romain <romainbousseau1@gmail.com>
2016-12-08 13:43:42 +0100

    added vaidation to work session

--- app/models/work_session.rb
+++ app/models/work_session.rb
@@ -2,4 +2,5 @@ class WorkSession < ApplicationRecord
   belongs_to :problem_user, :class_name => 'User', :foreign_key => 'problem_user_id'
   belongs_to :solver_user, :class_name => 'User', :foreign_key => 'solver_user_id'
   has_many :messages, dependent: :nullify
+  validates :starts_at, presence: true
 end
